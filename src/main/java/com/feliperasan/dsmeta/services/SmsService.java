e474ec45399abd282903d45823650ceae847ade9
Felipe Rafael Barbosa <rafaelt.ibarbosa@gmail.com>
2022-07-14 14:58:07 -0300

    Heroku

--- src/main/java/com/feliperasan/dsmeta/services/SmsService.java
+++ src/main/java/com/feliperasan/dsmeta/services/SmsService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.feliperasan.dsmeta.entities.Sale;
 import com.feliperasan.dsmeta.repositories.SaleRepository;
+
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
