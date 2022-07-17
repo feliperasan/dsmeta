e8ee8e581b1fe301ff20abf8ca7c5cff691ebe73
Felipe Rafael Barbosa <rafaelt.ibarbosa@gmail.com>
2022-07-17 08:04:49 -0300

    Axios, userEffect and fixes

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,10 +9,7 @@ function Header() {
           <img src={logo} alt="DSMeta" />
           <h1>DSMeta</h1>
           <p>
-            Desenvolvido por
-            <a href="https://www.instagram.com/feliperasan/" target="_blank">
-              @feliperasan
-            </a>
+            Desenvolvido por <a href="https://www.instagram.com/feliperasan/" target="_blank"> @feliperasan</a>
           </p>
         </div>
       </header>
